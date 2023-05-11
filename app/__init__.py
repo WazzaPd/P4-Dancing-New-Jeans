c1ca2dd9c03494684b9539faf2a39b7fce0c65eb
WazzaPd <Jwu31@stuy.edu>
2023-05-11 13:40:06 -0400

    in progress

--- app/__init__.py
+++ app/__init__.py
@@ -20,7 +20,7 @@ DB_FILE = "tables.db"
 db = sqlite3.connect(DB_FILE, check_same_thread=False)
 c = db.cursor()  # facilitate db ops -- you will use cursor to trigger db events
 
-c.execute("create table if not exists accounts(username TEXT, password TEXT);")
+c.execute("create table if not exists accounts(email TEXT, password TEXT);")
 db.commit()
 
 
@@ -33,16 +33,16 @@ def index():
     print(request)  # displays page request
     print("***DIAG: request.args ***")
     print(request.args)
-    # print("***DIAG: request.args['username']  ***")
-    # print(request.args['username'])
+    # print("***DIAG: request.args['email']  ***")
+    # print(request.args['email'])
     print("***DIAG: request.headers ***")
     print(request.headers)
 
-    if 'username' in session:
+    if 'email' in session:
         print("user is logged in as " +
-              session['username'] + ". Redirecting to /")
-        return render_template('index.html', username = True)
-    return render_template('index.html', username = False)
+              session['email'] + ". Redirecting to /")
+        return render_template('index.html', email = True)
+    return render_template('index.html', email = False)
 
 # REGISTER
 
@@ -50,9 +50,9 @@ def index():
 @app.route("/register", methods=['GET', 'POST'])
 def register():
     # Already logged in
-    if 'username' in session:
+    if 'email' in session:
         print("user is logged in as " +
-              session['username'] + " is already logged in. Redirecting to /")
+              session['email'] + " is already logged in. Redirecting to /")
         return redirect("/")
 
     # GET
@@ -61,15 +61,15 @@ def register():
 
     # POST
     if request.method == 'POST':
-        input_username = request.form['username']
+        input_email = request.form['email']
         input_password = request.form['password']
         input_confirm_password = request.form['confirm password']
 
     # if no registration info is inputted into the fields
-    if input_username.strip() == '' or input_password.strip() == '' or input_confirm_password.strip() == '':
+    if input_email.strip() == '' or input_password.strip() == '' or input_confirm_password.strip() == '':
         error_msg = ""
-        if input_username == '':
-            error_msg += "Please enter a username. \n"
+        if input_email == '':
+            error_msg += "Please enter a email. \n"
 
         if input_password == '':
             error_msg += "Please enter a password. \n"
@@ -85,18 +85,18 @@ def register():
         if input_password != input_confirm_password:
             return render_template('register.html', message="Passwords do not match. Please try again.")
 
-        # Checks for existing username in accounts table
-        var = (input_username,)
-        c.execute("select username from accounts where username=?", var)
+        # Checks for existing email in accounts table
+        var = (input_email,)
+        c.execute("select email from accounts where email=?", var)
 
-        # if there isn't an account associated with said username then create one
+        # if there isn't an account associated with said email then create one
         if not c.fetchone():
             c.execute("insert into accounts values(?, ?)",
-                      (input_username, input_password))
+                      (input_email, input_password))
             return render_template('login.html')
-        # if username is already taken
+        # if email is already taken
         else:
-            return render_template('register.html', message="Username is already taken. Please select another username.")
+            return render_template('register.html', message="email is already taken. Please select another email.")
 
 # login process
 
@@ -104,9 +104,9 @@ def register():
 @app.route("/login", methods=['GET', 'POST'])
 def login():
     # Already logged in
-    if 'username' in session:
+    if 'email' in session:
         print("user is logged in as " +
-              session['username'] + " is already logged in. Redirecting to /")
+              session['email'] + " is already logged in. Redirecting to /")
         return redirect("/")
 
     # GET
@@ -115,36 +115,36 @@ def login():
 
     # POST
     if request.method == 'POST':
-        input_username = request.form['username']
+        input_email = request.form['email']
         input_password = request.form['password']
 
     # Searches accounts table for user-password combination
-    c.execute("select username from accounts where username=? and password=?;",
-              (input_username, input_password))
+    c.execute("select email from accounts where email=? and password=?;",
+              (input_email, input_password))
 
     # login_check
     if c.fetchone():
         print("Login success!")
         if request.method == 'GET':  # For 'get'
-            # stores username in session
-            session['username'] = request.args['username']
+            # stores email in session
+            session['email'] = request.args['email']
 
         if request.method == 'POST':  # For 'post'
-            # stores username in session
-            session['username'] = request.form['username']
+            # stores email in session
+            session['email'] = request.form['email']
 
         return redirect("/")
 
     else:
         print("Login failed")
         error_msg = ''
-        username_check = "select username from accounts where username=?;"
-        password_check = "select username from accounts where password=?;"
+        email_check = "select email from accounts where email=?;"
+        password_check = "select email from accounts where password=?;"
 
-        # Username check
-        c.execute(username_check, (input_username,))
+        # email check
+        c.execute(email_check, (input_email,))
         if not c.fetchone():
-            error_msg += "Username is incorrect or not found. \n"
+            error_msg += "email is incorrect or not found. \n"
 
         # Password check
         c.execute(password_check, (input_password,))
@@ -159,8 +159,8 @@ def login():
 
 @app.route("/logout", methods=['GET', 'POST'])
 def logout():
-    # remove the username from the session if it's there
-    session.pop('username', None)
+    # remove the email from the session if it's there
+    session.pop('email', None)
     print("user has logged out. Redirecting to /login")
     return redirect("/")
 
@@ -173,17 +173,17 @@ def logout():
 #     print(request)
 #     print("***DIAG: request.args ***")
 #     print(request.form)  # displays entered info as dict
-#     print("***DIAG: request.args['username']  ***")
-#     print(request.form['username'])
+#     print("***DIAG: request.args['email']  ***")
+#     print(request.form['email'])
 #     print("***DIAG: request.headers ***")
 #     # metadata for the server about request+machine requesting
 #     print(request.headers)
-#     return f"Waaaa hooo HAAAH {request.form['username']}"  # response to a form submission
+#     return f"Waaaa hooo HAAAH {request.form['email']}"  # response to a form submission
 
 
 # @app.route("/index", methods=['GET', 'POST'])
 # def main():
-#     if 'username' in session:
+#     if 'email' in session:
 #         return render_template('index.html')
 #     else:
 #         print("user is not logged in. Redirecting to /login")
@@ -192,23 +192,23 @@ def logout():
 
 @app.route("/buy", methods=['GET', 'POST'])
 def buy():
-    if 'username' in session:
-        return render_template('buy.html', username = True)
-    return render_template('buy.html', username = False)
+    if 'email' in session:
+        return render_template('buy.html', email = True)
+    return render_template('buy.html', email = False)
     
 
 
 @app.route("/rent", methods=['GET', 'POST'])
 def rent():
-    if 'username' in session:
-        return render_template('rent.html', username = True)
-    return render_template('rent.html', username = False)
+    if 'email' in session:
+        return render_template('rent.html', email = True)
+    return render_template('rent.html', email = False)
 
 @app.route("/sell", methods=['GET', 'POST'])
 def sell():
-    if 'username' in session:
-        return render_template('sell.html', username = True)
-    return render_template('sell.html', username = False)
+    if 'email' in session:
+        return render_template('sell.html', email = True)
+    return render_template('sell.html', email = False)
 
 
 if __name__ == "__main__":  # false if this file imported as module
