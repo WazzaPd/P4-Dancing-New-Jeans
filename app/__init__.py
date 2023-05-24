62c1f3c0a2a5b194624be6b7de06b3b02e08eaf4
David Chen <dchen30@stuy.edu>
2023-05-24 02:21:29 -0400

    HELP I CANT W THIS ERROR MOVING EBVERYUTHING ITO ONE FILE ADBJASJKDA

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,10 @@
 # Dec 2023
 
 # import sqlite3  # for database building
+import sqlite3
+import base64
+from PIL import Image, ImageDraw
+import random
 from flask import Flask  # facilitate flask webserving
 from flask import render_template  # facilitate jinja templating
 from flask import request  # facilitate form submission
@@ -11,8 +15,8 @@ from flask import session  # facilitate user sessions
 from flask import redirect, url_for, jsonify  # to redirect to a different URL
 import os
 import requests
-from database import *
-from art import *
+# # from .database import *
+# from art import *
 
 dirname = os.path.dirname(__file__)
 attomKey = open(os.path.join(dirname, "keys/attom.txt")
@@ -290,6 +294,119 @@ def homes_by_zip(zip):
     response = requests.get(
         'https://api.gateway.attomdata.com/propertyapi/v1.0.0/assessment/detail', params=params, headers=headers)
     return response.json()
+
+
+# import os
+
+dirname = os.path.dirname(__file__)
+
+DB_FILE = os.path.join(dirname, "tables.db")
+
+db = sqlite3.connect(DB_FILE, check_same_thread=False)
+c = db.cursor()  # facilitate db ops -- you will use cursor to trigger db events
+
+# three tables: users, orders in cart, order history
+c.execute(
+    "CREATE TABLE IF NOT EXISTS users(email TEXT PRIMARY KEY, password TEXT)")
+# execute with relation to users table
+c.execute(
+    "CREATE TABLE IF NOT EXISTS order_history(email TEXT PRIMARY KEY, cart, history)")
+
+c.execute("CREATE TABLE IF NOT EXISTS orders(email TEXT, productName TEXT, date TEXT, quantity INT, productSKU TEXT, productPrice REAL, orderID INT PRIMARY KEY)")
+# TODO work this out later
+
+# c.execute("CREATE TABLE IF NOT EXISTS products (id INTEGER PRIMARY KEY, SKU TEXT, name TEXT, price REAL, description TEXT, image TEXT)")
+
+db.commit()  # save changes
+
+
+def check_email(email):
+    c.execute("SELECT * FROM users WHERE email = ?", (email,))
+    if c.fetchone():
+        return True
+    else:
+        return False
+
+
+def insert_account(email, password):
+    c.execute("INSERT INTO users (email, password) VALUES (?, ?)",
+              (email, password))
+    db.commit()
+
+
+def random_color():
+    return (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+
+
+def generate_house():
+    width, height = 256, 256
+    image = Image.new("RGB", (width, height), "white")
+    draw = ImageDraw.Draw(image)
+
+    # Draw a random building type
+    building_type = random.choice(["house", "apartment", "cabin", "mansion"])
+
+    if building_type == "house":
+        # draw windows
+        draw.rectangle([(64, 128), (192, 240)], fill=random_color())
+        draw.polygon([(64, 128), (192, 128), (128, 64)], fill=random_color())
+        # draw doors and chimney
+        draw.rectangle([(96, 192), (128, 240)], fill=random_color())
+        draw.rectangle([(160, 192), (192, 240)], fill=random_color())
+        draw.rectangle([(128, 64), (160, 96)], fill=random_color())
+    elif building_type == "apartment":
+
+        # draw windows
+        draw.rectangle([(64, 64), (192, 240)], fill=random_color())
+        for i in range(3):
+            draw.rectangle(
+                [(80, 80 + 48 * i), (176, 112 + 48 * i)], fill=random_color())
+        # draw doors
+        draw.rectangle([(96, 192), (128, 240)], fill=random_color())
+        draw.rectangle([(160, 192), (192, 240)], fill=random_color())
+
+    elif building_type == "cabin":
+
+        # draw windows
+        draw.rectangle([(64, 128), (192, 240)], fill=random_color())
+        draw.rectangle([(64, 64), (96, 96)], fill=random_color())
+        draw.rectangle([(160, 64), (192, 96)], fill=random_color())
+        # draw doors
+        draw.rectangle([(96, 192), (128, 240)], fill=random_color())
+        draw.rectangle([(160, 192), (192, 240)], fill=random_color())
+        draw.rectangle([(128, 64), (160, 96)], fill=random_color())
+
+    elif building_type == "mansion":
+
+        # draw windows
+        draw.rectangle([(64, 64), (192, 240)], fill=random_color())
+        for i in range(3):
+            draw.rectangle(
+                [(80, 80 + 48 * i), (176, 112 + 48 * i)], fill=random_color())
+        # draw doors
+        draw.rectangle([(96, 192), (128, 240)], fill=random_color())
+        draw.rectangle([(160, 192), (192, 240)], fill=random_color())
+        draw.rectangle([(128, 64), (160, 96)], fill=random_color())
+        # draw roof
+        draw.polygon([(64, 64), (192, 64), (128, 32)], fill=random_color())
+
+    # convert pil image to base64
+    # save image to temp file and get base64
+    image.save("temp.png")
+    with open("temp.png", "rb") as image_file:
+        # get base 64
+        encoded_string = base64.b64encode(image_file.read())
+        # print(encoded_string)
+        # remove temp file
+        # print(encoded_string)
+        # remove b prefix and quotes
+        encoded_string = str(encoded_string)[2:-1]
+        return encoded_string
+
+
+# # Generate and show a random house
+house_image = generate_house()
+# house_image.show()
 
 
 if __name__ == "__main__":  # false if this file imported as module
